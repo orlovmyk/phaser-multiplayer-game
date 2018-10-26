4ed4234c735e4c6494c562971bea01de4cd9be0f
orlovw <orlownikita265@gmail.com>
2018-10-26 21:08:45 +0300

    123

--- js/objects/Player.js
+++ js/objects/Player.js
@@ -16,7 +16,7 @@ class Player {
 		this.hitbox = this.scene.physics.add.sprite(x, y, 'empty_image');
 		this.hitbox.body.setSize(18, 35);
 
-		this.animation;
+		this.animation = 'idle';
 		this.animationList = [];
 
 		this.velocity = 140;
@@ -34,19 +34,22 @@ class Player {
 	}
 
 	loadAnimation(name, prefix, start, end, key){
-		var frameNames = this.scene.anims.generateFrameNames(name, {
+		let frameNames = this.scene.anims.generateFrameNames(name, {
                         start: start, 
                         end: end,
                         prefix: prefix, 
                         suffix: '.png'
                      });
+	
 		this.scene.anims.create({ key: key, frames: frameNames, frameRate: 10, repeat: -1 });
 	}
 
 	loadAnims(){
-		this.loadAnimation('kinght','knight_idle_', 0, 3, 'idle');
-
-		this.sprite.anims.play('idle');
+		this.loadAnimation('knight','knight_idle_', 0, 3, 'idle');
+		this.loadAnimation('knight','knight_run_up_', 0, 3, 'up');
+		this.loadAnimation('knight','knight_run_down_', 0, 3, 'down');
+		this.loadAnimation('knight','knight_run_right_', 0, 3, 'right');
+		this.loadAnimation('knight','knight_run_left_', 0, 3, 'left');
 	}
 
 	update(){
@@ -56,29 +59,120 @@ class Player {
 		this.sprite.setVelocity(0);
 		this.hitbox.setVelocity(0);
 
-		if (this.cursors.left.isDown)
+		if (this.cursors.left.isUp && 
+			this.cursors.right.isUp &&
+			this.cursors.up.isUp &&
+			this.cursors.down.isUp &&
+			this.animation != "idle"){
+					this.animation = "idle"
+					this.sprite.anims.play(this.animation);
+			}
+
+
+		if (this.cursors.up.isDown && this.cursors.left.isDown)
 		{
 			this.sprite.setVelocityX(-this.velocity);
 			this.hitbox.setVelocityX(-this.velocity);
-			//this.sprite.anims.play('walk-left');
+
+			this.sprite.setVelocityY(-this.velocity);
+			this.hitbox.setVelocityY(-this.velocity);
+			if(this.animation != "left_up") 
+				{
+					this.animation = "left_up"
+					this.sprite.anims.play("left");
+				};
+			return;
+		}
+		else if (this.cursors.up.isDown && this.cursors.right.isDown)
+		{
+			this.sprite.setVelocityX(this.velocity);
+			this.hitbox.setVelocityX(this.velocity);
+
+			this.sprite.setVelocityY(-this.velocity);
+			this.hitbox.setVelocityY(-this.velocity);
+			if(this.animation != "right_up") 
+				{
+					this.animation = "right_up"
+					this.sprite.anims.play("right");
+				};
+			return;
+		}
+
+		if (this.cursors.down.isDown && this.cursors.left.isDown)
+		{
+			this.sprite.setVelocityX(-this.velocity);
+			this.hitbox.setVelocityX(-this.velocity);
+
+			this.sprite.setVelocityY(this.velocity);
+			this.hitbox.setVelocityY(this.velocity);
+			if(this.animation != "left_down") 
+				{
+					this.animation = "left_down"
+					this.sprite.anims.play("left");
+				};
+			return;
+		}
+		else if (this.cursors.down.isDown && this.cursors.right.isDown)
+		{
+			this.sprite.setVelocityX(this.velocity);
+			this.hitbox.setVelocityX(this.velocity);
+
+			this.sprite.setVelocityY(this.velocity);
+			this.hitbox.setVelocityY(this.velocity);
+			if(this.animation != "right_down") 
+				{
+					this.animation = "right_down"
+					this.sprite.anims.play("right");
+				};
+			return;
+		}   
+
+		else if (this.cursors.left.isDown)
+		{
+			this.sprite.setVelocityX(-this.velocity);
+			this.hitbox.setVelocityX(-this.velocity);
+			if(this.animation != "left") 
+				{
+					this.animation = "left"
+					this.sprite.anims.play(this.animation);
+				};
+			return;
 		}
 		else if (this.cursors.right.isDown)
 		{
 			this.sprite.setVelocityX(this.velocity);
 			this.hitbox.setVelocityX(this.velocity);
-			//this.sprite.anims.play('walk-right');
+			if(this.animation != "right") 
+				{
+					this.animation = "right"
+					this.sprite.anims.play(this.animation);
+				};
+			return;
 		}
 
-		if (this.cursors.up.isDown)
+		else if (this.cursors.up.isDown)
 		{
 			this.sprite.setVelocityY(-this.velocity);
 			this.hitbox.setVelocityY(-this.velocity);
+			if(this.animation != "up" && this.animation != "left" && this.animation != "right") 
+				{
+					this.animation = "up"
+					this.sprite.anims.play(this.animation);
+				};
+			return;
 		}
 		else if (this.cursors.down.isDown)
 		{
 			this.sprite.setVelocityY(this.velocity);
 			this.hitbox.setVelocityY(this.velocity);
+			if(this.animation != "down" && this.animation != "left" && this.animation != "right") 
+				{
+					this.animation = "down"
+					this.sprite.anims.play(this.animation);
+				};
+			return;
 		}   
+
 	}
 
 }
